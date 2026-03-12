5b8c8950ea5ff31cadbccb8658efa3e9dec86647
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-03-12 11:42:34 +0530

    Add files via upload

---
+++ conditional statement python/7.traingle.py
@@ -0,0 +1,8 @@
+a=int(input())
+b=int(input())
+c=int(input())
+sum=a+b+c
+if sum==180 and a>0 and b>0 and c>0:
+    print("valid Triangle")
+else:
+    print("Not Traingle")
---
+++ conditional statement python/8.vowel.py
@@ -0,0 +1,10 @@
+ch=input()
+
+if len(ch)==1:
+   if ch in "aeioueAEIOU":
+       print ("vowel")
+   else:
+       print("not vowel")
+else:
+    print("enter the single character")
+       
